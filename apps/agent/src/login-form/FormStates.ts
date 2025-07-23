c05ad4cfe0278431c5b17b23cc388c9ae2faf53b
Montse Ortega <mortegag@redhat.com>
2025-07-23 08:50:54 +0200

    Revert "Remove agent-ui from this project (#169)"
    This reverts commit d643309148a575a4a9ebe06782f84b51c069c58c.
    
    Signed-off-by: Montse Ortega <mortegag@redhat.com>

---
+++ apps/agent/src/login-form/FormStates.ts
@@ -0,0 +1,9 @@
+export const enum FormStates {
+  CheckingStatus = "checkingStatus",
+  WaitingForCredentials = "waitingForCredentials",
+  Submitting = "submitting",
+  CredentialsAccepted = "credentialsAccepted",
+  CredentialsRejected = "credentialsRejected",
+  InvalidCredentials = "invalidCredentials",
+  GatheringInventory = "gatheringInventory"
+}
