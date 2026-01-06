0612ae2724227b9bb52ecea0c08243532eb9d306
Jonathan Kilzi <jkilzi@redhat.com>
2026-01-06 22:07:22 +0200

    fix: Update import statements in Context and Provider components to use .js file extensions
    - Changed import paths in Context.tsx and Provider.tsx from .ts to .js to ensure compatibility with module resolution.
    - This update aligns the code with the current file structure and improves consistency across the project.
    
    These changes enhance the maintainability and clarity of the codebase.
    
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- packages/ioc/src/Provider.tsx
+++ packages/ioc/src/Provider.tsx
@@ -1,9 +1,9 @@
 import type { Container } from "./Container.js";
 import { Context } from "./Context.js";
 
-export const Provider: React.FC<React.PropsWithChildren<{ container: Container }>> = (
-  props,
-) => {
+export const Provider: React.FC<
+  React.PropsWithChildren<{ container: Container }>
+> = (props) => {
   const { container, children } = props;
   return <Context.Provider value={container}>{children}</Context.Provider>;
 };
